a842eedac94c64adfc982f6e9a778f668976c999
binaridan <dabellian@gmail.com>
2019-08-05 16:01:54 -0500

    fist commit, hands set to work for oculus GO

---
+++ Assets/Scripts/FurnitureCheck.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FurnitureCheck : MonoBehaviour
+{
+
+private int layerMask;
+private HandController handController;
+public Transform rEmit;
+//public Transform lEmit;
+//public Animator LHandAnimator;
+public Animator RHandAnimator;
+//private float rGrab;
+//private float rFinger;
+//private float rThumb;
+//private float lGrab;
+//private float lFinger;
+//private float lThumb;
+private GameObject lastHit;
+
+private void Start() 
+    {
+        layerMask = 1 << 9;
+        handController = GetComponent<HandController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+         // Bit shift the index of the layer (8) to get a bit mask
+        
+        //rGrab = RHandAnimator.GetFloat("RGrab");
+        //rFinger = RHandAnimator.GetFloat("RFinger");
+        //rThumb = RHandAnimator.GetFloat("RThumb");
+
+        //lGrab = LHandAnimator.GetFloat("LGrab");
+        //lFinger = LHandAnimator.GetFloat("LFinger");
+        //lThumb = LHandAnimator.GetFloat("LThumb");
+
+        // This would cast rays only against colliders in layer 8.
+        // But instead we want to collide against everything except layer 8. The ~ operator does this, it inverts a bitmask.
+        //layerMask = layerMask;
+        if (handController.IsPointing(false))
+        {
+            
+            RaycastHit hit;
+            // Does the ray intersect any objects within the furniture layer
+            if (Physics.Raycast(rEmit.position, rEmit.TransformDirection(Vector3.forward), out hit, Mathf.Infinity, layerMask))
+            {
+                var info = hit.transform.gameObject.GetComponent<Info>();
+                
+                if (info != null)
+                {
+                    //Debug.Log("Did Hit");
+
+                    if (lastHit != info.obj && lastHit != null)
+                        lastHit.SetActive(false);
+
+                    lastHit = info.obj;
+                    info.obj.SetActive(true);
+                }
+
+                //Debug.DrawRay(rEmit.position, rEmit.TransformDirection(Vector3.forward) * hit.distance, Color.yellow);
+            }
+            else
+            {
+                if (lastHit != null)
+                {
+                    lastHit.SetActive(false);
+                    lastHit = null;
+                }
+                /*
+                toonSource.Find("toonLine").SetActive(false);
+                Debug.DrawRay(rEmit.position, rEmit.TransformDirection(Vector3.forward) * 1000, Color.white);
+                Debug.Log("Did not Hit");
+                if (info != null)
+                    info.obj.SetActive(false);
+                */
+            }
+        }
+        else
+        {
+            if (lastHit != null)
+            {
+                lastHit.SetActive(false);
+                lastHit = null;
+            }
+        }
+
+    }
+}
+
---
+++ Assets/_Design/Models/Avatar/HandController.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandController : MonoBehaviour 
+    
+
+{
+
+    private float lGrab = 0f;
+    private float lFinger = 0f;
+    private float lThumb = 0f;
+
+    [SerializeField]
+    private float rGrab = 0f;
+    [SerializeField]
+    private float rFinger = 0f;
+    [SerializeField]
+    private float rThumb = 0f;
+
+    public Transform handR;
+    public Transform handL;
+    private Animator LHandAnimator;
+    private Animator RHandAnimator;
+    public GameObject RHand;
+    public GameObject LHand;
+    public bool leftyMode = false;
+    public bool manual_override = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        LHandAnimator = LHand.GetComponent<Animator>();
+        RHandAnimator = RHand.GetComponent<Animator>();
+    }
+
+    // Updates the values for hand positions based on Oculus Input
+    void UpdateGestures()
+    {
+
+        if (!manual_override)
+        {
+            rGrab = (leftyMode) ? OVRInput.Get(OVRInput.Axis1D.PrimaryHandTrigger) : OVRInput.Get(OVRInput.Axis1D.SecondaryHandTrigger);
+            rFinger = (leftyMode) ? OVRInput.Get(OVRInput.Axis1D.PrimaryIndexTrigger) : OVRInput.Get(OVRInput.Axis1D.SecondaryIndexTrigger);
+            rThumb = (leftyMode) ? (OVRInput.Get(OVRInput.NearTouch.PrimaryThumbButtons) ? 1f : 0f) : (OVRInput.Get(OVRInput.NearTouch.SecondaryThumbButtons) ? 0f : 1f);
+
+            lGrab = (!leftyMode) ? OVRInput.Get(OVRInput.Axis1D.PrimaryHandTrigger) : OVRInput.Get(OVRInput.Axis1D.SecondaryHandTrigger);
+            lFinger = (!leftyMode) ? OVRInput.Get(OVRInput.Axis1D.PrimaryIndexTrigger) : OVRInput.Get(OVRInput.Axis1D.SecondaryIndexTrigger);
+            lThumb = (!leftyMode) ? (OVRInput.Get(OVRInput.NearTouch.PrimaryThumbButtons) ? 1f : 0f) : (OVRInput.Get(OVRInput.NearTouch.SecondaryThumbButtons) ? 0f : 1f);
+        }
+
+        RHandAnimator.SetFloat("RGrab", rGrab);
+        RHandAnimator.SetFloat("RFinger", rFinger);
+        RHandAnimator.SetFloat("RThumb", rThumb);
+
+        LHandAnimator.SetFloat("LGrab", lGrab);
+        LHandAnimator.SetFloat("LFinger", lFinger);
+        LHandAnimator.SetFloat("LThumb", lThumb);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateGestures();
+    }
+
+    public bool IsPointing(bool isLeft)
+    {
+        if (isLeft)
+        {
+            if (lGrab > 0.5f && lFinger < 0.5f)
+                return true;
+        }
+        else
+        {
+            if (rGrab > 0.5f && rFinger < 0.5f)           
+            {
+                Debug.Log("is Pointing");
+                return true;
+            } 
+        }
+        return false;
+    }
+}
